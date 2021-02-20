4b038e021e13c89fe3b96410530849f49249c567
AngelaSophia <ayugisophiaangela@gmail.com>
2021-02-20 23:00:40 +0300

    assignment

---
+++ src/assignment.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var fullname ="AYUGI SOPHIA ANGELA"
+    println(fullname)
+
+    var age=23
+    println(age)
+
+    var phoneNumber="0758183543"
+    println(phoneNumber)
+
+    var weight=58.6
+    println(weight)
+
+    var citizenship: Boolean= true
+    println(citizenship)
+
+
+
+
+
+}
