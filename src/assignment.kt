82346884d64e657f7a803b9ff60942eff97eec45
AngelaSophia <ayugisophiaangela@gmail.com>
2021-02-28 12:16:43 +0300

    second assignment

--- src/assignment.kt
+++ src/assignment.kt
@@ -1,22 +1,23 @@
-fun main() {
-    var fullname ="AYUGI SOPHIA ANGELA"
-    println(fullname)
-
-    var age=23
-    println(age)
-
-    var phoneNumber="0758183543"
-    println(phoneNumber)
-
-    var weight=58.6
-    println(weight)
-
-    var citizenship: Boolean= true
-    println(citizenship)
-
-
-
-
+fun main () {
+    var name="Hello Sophia Ayugi"
+    println(name)
+    var num1=10
+    var num2=3
+    var modulus=num1%num2
+    println(modulus)
+    var nub1=10
+    var nub2=20
+    var nub3=30
+    var nub4=40
+    var sum=nub1+nub2+nub3+nub4
+    println(sum)
+    var intrestingfact="funny"
+    println(intrestingfact)
 
 
 }
+
+
+
+
+
